cdf23f989f0176a8dca9d94447155e3dde8ad153
mạnh Nguyễn <nguyenmanh2004devgame@gmail.com>
2025-10-21 15:24:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4746898573-a49e5",
+  "appId": "1:828027854047:web:a236dd60099962cba09d6a",
+  "apiKey": "AIzaSyBnlKmVTQ9M-comeN0Jyfuyado6M5c85Vw",
+  "authDomain": "studio-4746898573-a49e5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "828027854047"
+};
