bb5f4bd3456a3e429ed1992ccfaf0c1625818857
mạnh Nguyễn <nguyenmanh2004devgame@gmail.com>
2025-10-21 17:44:54 +0000

    BkguS3oS48hEyhzSJnWY1IsfKr13lJZiM9lEZ7ELRGs nhầm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Unity Codex",
   description: "A modern blog for collecting and sharing Unity C# code snippets.",
   verification: {
-    google: 'BkguS3oS48hEyhzSJ',
+    google: 'BkguS3oS48hEyhzSJnWY1IsfKr13lJZiM9lEZ7ELRGs',
   },
 };
 
