af415b019a439c9e09647fa72e7507f10a369de6
mạnh Nguyễn <nguyenmanh2004devgame@gmail.com>
2025-10-21 08:53:56 +0000

    vẫn thế

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -32,6 +32,7 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/lib/tag-colors.ts'
   ],
   theme: {
     container: {
